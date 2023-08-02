1e0fdd78dd5711345b9e7711d76a469fc3d055ff
nphoang665 <phihoang665@gmail.com>
2023-08-02 19:23:24 +0700

    #XemHoaDon

--- QuanLyKhachSan/Form/frm_ThongTinPhong.designer.cs
+++ QuanLyKhachSan/Form/frm_ThongTinPhong.designer.cs
@@ -222,7 +222,7 @@
             this.btn_ThanhToan.Name = "btn_ThanhToan";
             this.btn_ThanhToan.Size = new System.Drawing.Size(124, 40);
             this.btn_ThanhToan.TabIndex = 50;
-            this.btn_ThanhToan.Text = "Thanh toán";
+            this.btn_ThanhToan.Text = "Xem hóa đơn";
             this.btn_ThanhToan.UseVisualStyleBackColor = false;
             this.btn_ThanhToan.Click += new System.EventHandler(this.btn_ThanhToan_Click);
             // 
