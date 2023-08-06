12fc9f92c777ad626ef4024b68b19b1637fd0bc5
nphoang665 <phihoang665@gmail.com>
2023-08-06 16:10:27 +0700

    #DoneFixDeForm_TrangChu+DoiMatKhau

--- QuanLyKhachSan/Form/frm_DoiMatKhau.cs
+++ QuanLyKhachSan/Form/frm_DoiMatKhau.cs
@@ -51,10 +51,7 @@ namespace QuanLyKhachSan
             bool doimk = bus.DoiMatKhau(_tk, txt_MatKhauCu.Text, txt_MatKhauMoi.Text);
             if (doimk)
             {
-                this.Hide();
-                frm_TrangChu frm_tc = new frm_TrangChu(_tk);
-              
-                frm_tc.Close();
+                frm_TrangChu.ViDu.Hide();
                 frm_DangNhap frm = new frm_DangNhap();
                 frm.ShowDialog();
                 this.Close();
